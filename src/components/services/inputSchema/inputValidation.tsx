8f245896d9eba29a779d2e46135af1fd0cf97778
unknown <jazzygee36gmail.com>
2023-04-15 22:31:20 +0100

    schema

--- src/components/services/inputSchema/inputValidation.tsx
+++ src/components/services/inputSchema/inputValidation.tsx
@@ -1,16 +1,20 @@
 import { z } from "zod";
 
-
 export const loginInput = z.object({
-  email: z.string().email('email is required'),
-  password: z.string().min(6, 'password is required').max(18),
+  email: z.string().email("email is required"),
+  password: z.string().min(6, "password is required").max(18),
 });
 
-
-export const SignupSchema = z.object({
-  email: z.string().email('email is required'),
-  password: z.string().min(6, 'password is required').max(18),
-  username: z.string().min(1, 'username is required').max(18),
-  confirmPwd: z.string().min(6, 'confirm password must be same with password').max(18),
-
-});
+export const SignupSchema = z
+  .object({
+    email: z.string().email("email is required"),
+    password: z.string().min(6, "password must not be less than 6 characters"),
+    username: z.string().min(1, "username is required"),
+    confirmPwd: z
+      .string()
+      .min(6, "confirm password must be same with password"),
+  })
+  .refine((data) => data.password === data.confirmPwd, {
+    message: "Password doesn't match",
+    path: ["confirmPwd"],
+  });
